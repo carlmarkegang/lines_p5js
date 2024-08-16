410d3368564ec7c8361752cf3f4b0790da92ec19
Carl Markegång <huledule@hotmail.com>
2024-08-16 08:11:50 +0200

    a

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 var resolution = 512;
 
 var ground = [];
-var groundSegments = 20;
+var groundSegments = 30;
 var spacing = resolution / (groundSegments);
 
 var spider = [];
@@ -45,9 +45,15 @@ function draw() {
     let closest1;
     let closest2;
     let closest3;
-    let minDistance1 = 200;
-    let minDistance2 = 200;
-    let minDistance3 = 200;
+    let closest4;
+    let closest5;
+    let closest6;
+    let minDistance1 = 300;
+    let minDistance2 = 300;
+    let minDistance3 = 300;
+    let minDistance4 = 300;
+    let minDistance5 = 300;
+    let minDistance6 = 300;
 
     for (let i = 0; i < groundSegments + 2; i++) {
         stroke(color(137, 207, 240), 0, 0);
@@ -68,6 +74,10 @@ function draw() {
 
         var closest1WasSet = false;
         var closest2WasSet = false;
+        var closest3WasSet = false;
+        var closest4WasSet = false;
+        var closest5WasSet = false;
+        var closest6WasSet = false;
         if (distance < minDistance1) {
             minDistance1 = distance;
             closest1 = ground[i];
@@ -81,6 +91,22 @@ function draw() {
         if (distance < minDistance3 && closest1WasSet == false && closest2WasSet == false) {
             minDistance3 = distance;
             closest3 = ground[i];
+            closest3WasSet = true;
+        }
+        if (distance < minDistance4 && closest1WasSet == false && closest2WasSet == false && closest3WasSet == false) {
+            minDistance4 = distance;
+            closest4 = ground[i];
+            closest4WasSet = true;
+        }
+        if (distance < minDistance5 && closest1WasSet == false && closest2WasSet == false && closest3WasSet == false && closest4WasSet == false) {
+            minDistance5 = distance;
+            closest5 = ground[i];
+            closest5WasSet = true;
+        }
+        if (distance < minDistance6 && closest1WasSet == false && closest2WasSet == false && closest3WasSet == false && closest4WasSet == false && closest5WasSet == false) {
+            minDistance6 = distance;
+            closest6 = ground[i];
+            closest6WasSet = true;
         }
 
 
@@ -92,10 +118,13 @@ function draw() {
     fill(color(137, 207, 240), 0, 0);
     rect(spider[0].x, spider[0].y, spider[0].width, spider[0].width);
 
-    if (closest1 && closest2 && closest3) {
+    if (closest1 && closest2 && closest3 && closest4 && closest5 && closest6) {
         line(spider[0].x, spider[0].y, closest1.x_end, closest1.y_end);
         line(spider[0].x, spider[0].y, closest2.x_end, closest2.y_end);
         line(spider[0].x, spider[0].y, closest3.x_end, closest3.y_end);
+        line(spider[0].x, spider[0].y, closest4.x_end, closest4.y_end);
+        line(spider[0].x, spider[0].y, closest5.x_end, closest5.y_end);
+        line(spider[0].x, spider[0].y, closest6.x_end, closest6.y_end);
     }
 
 
